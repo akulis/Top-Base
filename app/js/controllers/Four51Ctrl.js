65e3472a76ee8f9a573b6756e158dbfc3f7cdb7e
Andy K <andy@kulis.app>
2025-01-25 15:42:25 -0600

    Enhance Four51Ctrl with user session timeout management and improved login handling

--- app/js/controllers/Four51Ctrl.js
+++ app/js/controllers/Four51Ctrl.js
@@ -1,12 +1,17 @@
-four51.app.controller('Four51Ctrl', ['$scope', '$route', '$location', '$451', 'User', 'Order', 'Security', 'OrderConfig', 'Category', 'AppConst','XLATService', 'GoogleAnalytics', 'SpendingAccount',
-function ($scope, $route, $location, $451, User, Order, Security, OrderConfig, Category, AppConst, XLATService, GoogleAnalytics, SpendingAccount) {
+four51.app.controller('Four51Ctrl', ['$scope', '$route', '$rootScope', '$timeout', '$document', '$window', '$location', '$451', 'User', 'Order', 'Security', 'OrderConfig', 'Category', 'AppConst','XLATService', 'GoogleAnalytics', 'SpendingAccount',
+function ($scope, $route, $rootScope, $timeout, $document, $window, $location, $451, User, Order, Security, OrderConfig, Category, AppConst, XLATService, GoogleAnalytics, SpendingAccount) {
 	$scope.AppConst = AppConst;
 	$scope.scroll = 0;
 	$scope.isAnon = $451.isAnon; //need to know this before we have access to the user object
 	$scope.Four51User = Security;
 	if ($451.isAnon && !Security.isAuthenticated()) {
-		User.login(function () {
-			$route.reload();
+		var tempUser = {
+			Username: null,
+			Password: null,
+			Email: null
+		};
+		User.login(tempUser,function (u) {
+			location.reload();
 		});
 	}
 
@@ -28,7 +33,7 @@ function ($scope, $route, $location, $451, User, Order, Security, OrderConfig, C
                 $scope.user.Culture.CurrencyPrefix = XLATService.getCurrentLanguage(user.CultureUI, user.Culture.Name)[1];
                 $scope.user.Culture.DateFormat = XLATService.getCurrentLanguage(user.CultureUI, user.Culture.Name)[2];
 
-                //SPENDING ACCOUNTS NAV
+                //SPENDING ACCOUNTS NAV $ak
                 SpendingAccount.query(function(data) {
     		       $scope.userSpendingAccounts = data;
                 });
@@ -75,4 +80,37 @@ function ($scope, $route, $location, $451, User, Order, Security, OrderConfig, C
 	});
 	$scope.$on("$routeChangeSuccess", init);
     $scope.$on('event:auth-loginRequired', cleanup);
+
+	// Timeout timer value
+	var TimeOutTimerValue = 15*60*1000;
+
+	// Start a timeout
+	var TimeOut_Thread = $timeout(function(){ LogoutByTimer() } , TimeOutTimerValue);
+	var bodyElement = angular.element($document);
+
+	angular.forEach(['keydown', 'keyup', 'click', 'mousemove', 'DOMMouseScroll', 'mousewheel', 'mousedown', 'touchstart', 'touchmove', 'scroll', 'focus'],
+		function(EventName) {
+			bodyElement.bind(EventName, function (e) { TimeOut_Resetter(e) });
+		});
+
+	function LogoutByTimer(){
+		User.logout($scope.user, function(u){
+			if ($scope.isAnon) {
+				$timeout(function () {
+					$location.path("/catalog");
+					location.reload(true);
+				}, 500);
+			}
+		}, function(ex){
+			console.log(ex.Message);
+		});
+	}
+
+	function TimeOut_Resetter(e){
+		/// Stop the pending timeout
+		$timeout.cancel(TimeOut_Thread);
+
+		/// Reset the timeout
+		TimeOut_Thread = $timeout(function(){ LogoutByTimer() } , TimeOutTimerValue);
+	}
 }]);
